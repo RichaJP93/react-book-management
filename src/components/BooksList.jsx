56e896eb845e28a8ae3ae30f82f4cd0624530e79
Joseph Richardson <richardsonjp93@gmail.com>
2021-04-20 12:10:43 +0100

    added booklist component

--- src/components/BooksList.jsx
+++ src/components/BooksList.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from 'react';
+import _ from 'lodash';
+import Book from './Book';
 
-const BooksList = () => (
-  <h2>List of Books</h2>
-)
+const BooksList = ({books, setBooks}) => {
+
+  const handleRemoveBook = (id) => {
+    setBooks(books.filter((book) => book.id !== id))
+  };
+
+  return (
+    <React.Fragment>
+      <div className='books-list'>
+        {!_.isEmpty(books) ? (
+          books.map((book) => (
+            <Book key={book.id} {...book} handleRemoveBook={handleRemoveBook} />
+          ))
+        ) : (
+          <p className='message'>No books available. Please add some books</p>
+        )}
+      </div>
+    </React.Fragment>
+  )
+
+}
 
 export default BooksList
